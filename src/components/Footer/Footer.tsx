f4b06e5f855c695e6d2ab0c38033abbcb3db7499
Just In <jkhclimited@gmail.com>
2023-07-31 20:43:28 -0400

    fixed some text and removed redundant files

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: FC = () => {
         <div className="footerArea">
             <div className="footerText">
                 <p className="footerLine">Created by Raravin and Rafu.</p>
-                <p className="footerLine">All images owned by Weebs of the Shore.</p>
+                <p className="footerLine">All card images owned by Weebs of the Shore.</p>
                 <p className="footerLine">Please direct any questions, issues, and content creation requests to Raravin or Rafutalia on discord.</p>
             </div>
         </div>
