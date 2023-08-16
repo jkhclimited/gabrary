5b51b452ce59f62993d5e0eada216491d02f22cb
Just In <jkhclimited@gmail.com>
2023-08-16 09:10:30 -0400

    added some text under blurb

--- src/pages/FrontPage/FrontPage.tsx
+++ src/pages/FrontPage/FrontPage.tsx
@@ -16,6 +16,7 @@ const FrontPage: FC = () => {
                     <p className='latest-article-header'>Check out the latest article below!</p>
                     <Link to={`/articles/${articles[articles.length - 1].id}`} className='latest-articleLink'>
                         <img className='latest-articleIMG' src={process.env.PUBLIC_URL + `/Articles_Images/${articles[articles.length - 1].mainIMG}`} alt=''></img>
+                        {`${articles[articles.length - 1].title}`}
                     </Link>
                 </div>
             </div>
