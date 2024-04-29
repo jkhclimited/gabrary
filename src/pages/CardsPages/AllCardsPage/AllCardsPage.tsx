1de47800fb1e320749410aa70198a638af862cf9
Raravin-Keebs <58320134+Raravin-Keebs@users.noreply.github.com>
2024-04-29 09:38:30 -0400

    Add files via upload

--- src/pages/CardsPages/AllCardsPage/AllCardsPage.tsx
+++ src/pages/CardsPages/AllCardsPage/AllCardsPage.tsx
@@ -137,8 +137,8 @@ const AllCardsPage: FC = () => {
                 <p className="centerText">Card of the Day</p>
                 <div className="flexCardImgs">
                     <div className="text-row">
-                        <p className="cardImgBox"><img className="cardImg" onClick={() => showLightbox('144')} src={process.env.PUBLIC_URL + `/Spoilers_Images/144.jpg`} alt="cotd" /></p>
-                        <p style={{ display: (allCardsState.showing ? 'block' : 'none' ) }} className="centerText">Favorable Winds</p>
+                        <p className="cardImgBox"><img className="cardImg" onClick={() => showLightbox('6.1')} src={process.env.PUBLIC_URL + `/Spoilers_Images/6.1.jpg`} alt="cotd" /></p>
+                        <p style={{ display: (allCardsState.showing ? 'block' : 'none' ) }} className="centerText">Tristan, Shadowreaver</p>
                     </div>
                 </div>
             </div>
