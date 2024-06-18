83638b49f1c7ba464678fb3083f65d2260b758bc
Raravin-Keebs <58320134+Raravin-Keebs@users.noreply.github.com>
2024-06-17 23:30:27 -0400

    Add files via upload

--- src/pages/CardsPages/AllCardsPage/AllCardsPage.tsx
+++ src/pages/CardsPages/AllCardsPage/AllCardsPage.tsx
@@ -153,8 +153,8 @@ const AllCardsPage: FC = () => {
                 <p className="centerText">Card(s) of the Day</p>
                 <div className="flexCardImgs">
                     <div className="text-row">
-                        <p className="cardImgBox"><img className="cardImg" onClick={() => showLightbox('008)} src={process.env.PUBLIC_URL + `/Spoilers_Images/008.jpg`} alt="cotd" /></p>
-                        <p style={{ display: (allCardsState.showing ? 'block' : 'none' ) }} className="centerText">Creeping Torment</p>
+                        <p className="cardImgBox"><img className="cardImg" onClick={() => showLightbox('008')} src={process.env.PUBLIC_URL + `/Spoilers_Images/174.jpg`} alt="cotd" /></p>
+                        <p style={{ display: (allCardsState.showing ? 'block' : 'none' ) }} className="centerText">Convoking Slime</p>
                     </div>
                 </div>
             </div>
